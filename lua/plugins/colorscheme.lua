059b3f3f362cf7a48867d5f6ddceabca548d0392
pc <nikitamilovatskiy@gmail.com>
2024-02-21 01:45:05 +0300

    feat: move colorschemes to one file

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"catppuccin/nvim",
+
+		enabled = false,
+
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			-- require("catppuccin").setup({ flavour = "latte" })
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+
+		-- enabled = false,
+
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- vim.cmd("colorscheme kanagawa-wave")
+			vim.cmd("colorscheme kanagawa-dragon")
+			-- vim.cmd("colorscheme kanagawa-lotus")
+		end,
+	},
+}
