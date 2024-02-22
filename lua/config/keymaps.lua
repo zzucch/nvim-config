95a8272e52edc6db4a915ed4de1e5cae377775f2
pc <nikitamilovatskiy@gmail.com>
2024-02-22 14:50:42 +0300

    feat: capital j, k mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,5 +15,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- ex
 vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
 
+-- fav ctrl shift arrows
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
