f0c3308a7e01fce7defca3e6ad0e0a172d08e11c
zzucch <nikitamilovatskiy@gmail.com>
2024-02-24 20:34:52 +0300

    feat: colorscheme+lsp minor features

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,27 @@
 return {
 	{
 		"catppuccin/nvim",
-
 		--	enabled = false,
-
 		lazy = false,
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+				term_colors = true,
+				styles = {
+					comments = {},
+					conditionals = {},
+				},
+			})
+
 			-- require("catppuccin").setup({ flavour = "latte" })
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 	{
 		"rebelot/kanagawa.nvim",
-
 		enabled = false,
-
 		lazy = false,
 		priority = 1000,
 		config = function()
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,21 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			-- install synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "markdown" },
+			},
+			indent = { enable = true },
+		})
 
-    vim.filetype.add({
-      pattern = { [".*/hypr.*%.conf"] = "hyprlang" },
-    })
-  end,
+		vim.filetype.add({
+			pattern = { [".*/hypr.*%.conf"] = "hyprlang" },
+		})
+	end,
 }
