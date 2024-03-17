cbc11e099ef15c1a5ce0bbeb2ae7cc3874167b5f
zzucch <nikitamilovatskiy@gmail.com>
2024-03-17 18:41:00 +0300

    feat: add shellharden

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,6 +10,7 @@ return {
       opts.sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.shellharden,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.isort,
