63b4efe5b1cb20cf2e03d83a11dbdcaadf480d60
zzucch <nikitamilovatskiy@gmail.com>
2024-03-17 03:34:17 +0300

    feat: add auto download for none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,55 +1,56 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "j-hui/fidget.nvim",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
-  },
-  config = function()
-    local cmp = require("cmp")
-    local cmp_lsp = require("cmp_nvim_lsp")
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"j-hui/fidget.nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
 
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-    require("fidget").setup()
-    require("mason").setup()
+    require("luasnip.loaders.from_vscode").lazy_load()
+		require("fidget").setup()
+		require("mason").setup()
 
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "bashls",
-        "gopls",
-      },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup({})
-        end,
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-              },
-            },
-          })
-        end,
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"bashls",
+				"gopls",
+			},
+			handlers = {
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({})
+				end,
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
 				["gopls"] = function()
 					local lspconfig = require("lspconfig")
 					lspconfig.gopls.setup({
@@ -62,9 +63,10 @@ return {
 									generate = true,
 									regenerate_cgo = true,
 									run_govulncheck = true,
-									test = true,
+									-- legacy:
+									-- test = true,
 									tidy = true,
-									upgrade_dependency = true,
+									upgrnilnessade_dependency = true,
 									vendor = true,
 								},
 								hints = {
@@ -78,8 +80,7 @@ return {
 								},
 								analyses = {
 									fieldalignment = true,
-									nilness = true,
-									unusedparams = true,
+									unusedvariable = true,
 									unusedwrite = true,
 									useany = true,
 								},
@@ -99,41 +100,41 @@ return {
 						},
 					})
 				end,
-      },
-    })
+			},
+		})
 
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-      }, {
-        { name = "buffer" },
-      }),
-    })
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-    vim.diagnostic.config({
-      -- update_in_insert = true,
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    })
-  end,
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,43 @@
 return {
-  "nvimtools/none-ls.nvim",
-  opts = function(_, opts)
-    local null_ls = require("null-ls")
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
 
-    opts.root_dir = opts.root_dir
-        or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+      opts.root_dir = opts.root_dir
+          or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
 
-    opts.sources = {
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.shfmt,
-      null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formatting.isort,
-      null_ls.builtins.code_actions.gomodifytags,
-      null_ls.builtins.code_actions.impl,
-      null_ls.builtins.formatting.goimports,
-      null_ls.builtins.formatting.gofumpt,
-    }
+      opts.sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.code_actions.gomodifytags,
+        null_ls.builtins.code_actions.impl,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.diagnostics.golangci_lint.with({
+          extra_args = { "--enable-all" },
+        }),
+        null_ls.builtins.diagnostics.staticcheck,
+      }
 
-    vim.keymap.set({ "n", "v" }, "<C-A-l>", vim.lsp.buf.format, {})
-  end,
+      vim.keymap.set({ "n", "v" }, "<C-A-l>", vim.lsp.buf.format, {})
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = nil,
+        automatic_installation = true,
+      })
+    end,
+  },
 }
