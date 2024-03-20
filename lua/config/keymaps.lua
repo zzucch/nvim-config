fc7568938e8037610a19f1b71d8f4bef14bf64a7
zzucch <nikitamilovatskiy@gmail.com>
2024-03-20 12:57:05 +0300

    feat: add leader+ee for go errors

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,6 +22,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
 -- lsp
 vim.keymap.set({ "n", "v" }, "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
