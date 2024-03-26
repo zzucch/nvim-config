d5d0ad78a7c3b4d69670b796df29a41f9b175c32
zzucch <nikitamilovatskiy@gmail.com>
2024-03-26 23:16:15 +0300

    fix: remove sqlfluff because i dont want to deal with sqlsp

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,46 +1,43 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		opts = function(_, opts)
+			local null_ls = require("null-ls")
 
-      opts.root_dir = opts.root_dir
-          or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+			opts.root_dir = opts.root_dir
+				or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
 
-      opts.sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.formatting.shellharden,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.impl,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.diagnostics.golangci_lint.with({
-          -- extra_args = { "--enable-all" },
-        }),
-        null_ls.builtins.diagnostics.staticcheck,
-        null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.diagnostics.sqlfluff.with({
-          extra_args = { "--dialect", "postgres" },
-        }),
-      }
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-      require("mason-null-ls").setup({
-        ensure_installed = nil,
-        automatic_installation = true,
-      })
-    end,
-  },
+			opts.sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.formatting.shellharden,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.code_actions.gomodifytags,
+				null_ls.builtins.code_actions.impl,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.diagnostics.golangci_lint.with({
+					-- extra_args = { "--enable-all" },
+				}),
+				null_ls.builtins.diagnostics.staticcheck,
+				null_ls.builtins.diagnostics.hadolint,
+			}
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
