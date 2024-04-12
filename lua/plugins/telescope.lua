19279d7c779993135f082401d90acc4df426ab56
zzucch <nikitamilovatskiy@gmail.com>
2024-04-12 11:18:18 +0300

    feat: remap alt on ctrl

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-			vim.keymap.set("n", "<A-p>", builtin.git_files, {})
+			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 
 			vim.keymap.set("n", "<leader>ps", function()
 				builtin.grep_string({ search = vim.fn.input("grep > ") })
