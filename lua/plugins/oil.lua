3c0f5a68e0a7743de67e1f57b2687af3ae5768e6
zzucch <nikitamilovatskiy@gmail.com>
2024-05-16 22:22:21 +0300

    feat: set netrw as a default file explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup({
+        default_file_explorer = false,
         columns = { "icon" },
         keymaps = {},
         view_options = {
