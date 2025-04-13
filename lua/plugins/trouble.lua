05739e3fe062a26a880872779b213e6f97444b48
zzucch <nikitamilovatskiy@gmail.com>
2025-04-13 23:39:20 +0300

    feat: add trouble error-only toggle

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -7,5 +7,9 @@ return {
             "<leader>tt",
             "<cmd>Trouble diagnostics toggle<cr>",
         },
+        {
+            "<leader>te",
+            "<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.ERROR<cr>",
+        },
     },
 }
